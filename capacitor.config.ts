269700b2087260ec5855bcbe8d9af15c09d463ab
Alessandro Traversari <alessandro.traversari@shark-soft.com>
2025-04-13 21:32:31 +0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'speak-up-on-the-way',
+  webDir: 'www'
+};
+
+export default config;
