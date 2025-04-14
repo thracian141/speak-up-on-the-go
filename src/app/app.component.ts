d92ada4ae19ce1610abef1bf436e7fe3ef67d20e
Alessandro Traversari <alessandro.traversari@shark-soft.com>
2025-04-14 18:04:32 +0300

    aa

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,11 @@
 import { Component } from '@angular/core';
-import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonButtons, IonTitle, IonButton, IonIcon, IonMenuButton, IonImg, IonLabel } from '@ionic/angular/standalone';
+import { IonApp, IonRouterOutlet, IonHeader, IonToolbar, IonButtons, IonTitle, IonButton, IonIcon, IonMenuButton, IonImg, IonLabel, IonContent } from '@ionic/angular/standalone';
 
 @Component({
   selector: 'app-root',
   templateUrl: 'app.component.html',
-  imports: [IonLabel, IonImg, IonIcon, IonButton, IonTitle, IonButtons, IonToolbar, IonHeader, IonApp, IonRouterOutlet, IonMenuButton],
+  imports: [IonContent, IonLabel, IonImg, IonButton, IonTitle, IonButtons, IonToolbar, IonHeader, IonApp, IonRouterOutlet],
+  styleUrls: ['app.component.scss'],
 })
 export class AppComponent {
   constructor() {}
