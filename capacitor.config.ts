7b50cd1971df31ef361a60140203f10820138d50
Stoyan <158443318+Derkurto@users.noreply.github.com>
2025-04-15 01:51:54 +0300

    renaming app -

--- capacitor.config.ts
+++ capacitor.config.ts
@@ -2,7 +2,7 @@ import type { CapacitorConfig } from '@capacitor/cli';
 
 const config: CapacitorConfig = {
   appId: 'io.ionic.starter',
-  appName: 'speak-up-on-the-way',
+  appName: 'speak-up-on-the-go',
   webDir: 'www'
 };
 
